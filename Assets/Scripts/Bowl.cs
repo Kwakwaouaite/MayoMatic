57e7a2435326e8744615a7f926fe3d507de1dc4e
Kwakwaouaite <pamart.pierrick@hotmail.fr>
2020-12-17 21:49:01 +0100

    Add art for the bowl

--- Assets/Scripts/Bowl.cs
+++ Assets/Scripts/Bowl.cs
@@ -13,14 +13,26 @@ namespace MayoMatic
         float m_AngleSpeed = 180.0f;
 
         [SerializeField]
-        LineRenderer m_PlayerLineRenderer;
+        Transform m_PlayerTransform;
 
         [SerializeField]
         Transform m_TargetTransform;
 
         float m_TargetAngle = 0;
         float m_CurrentPlayerAngle = 0;
+
         float m_CurrentGapPercentage = 0;
+        float m_FramePlayerAngleSpeed = 0;
+
+        Vector3 m_PlayerInitialRotation;
+
+        private void Start()
+        {
+            if (m_PlayerTransform)
+            {
+                m_PlayerInitialRotation = m_PlayerTransform.localEulerAngles; 
+            }
+        }
 
         // Update is called once per frame
         void Update()
@@ -45,14 +57,24 @@ namespace MayoMatic
             float joystickX = Input.GetAxis("Horizontal");
             float joystickY = Input.GetAxis("Vertical");
 
-            m_CurrentPlayerAngle = Mathf.Atan2(joystickY, joystickX);
+            float newPlayerAngle = Mathf.Atan2(joystickY, joystickX);
+
+            float angleThisFrame = newPlayerAngle - m_CurrentPlayerAngle;
+
+            if (angleThisFrame > Mathf.PI)
+            {
+                angleThisFrame -= Mathf.PI;
+            }
+
+            m_FramePlayerAngleSpeed = angleThisFrame / Time.deltaTime;
+            Debug.Log("Speed: " + m_FramePlayerAngleSpeed);
+
+            m_CurrentPlayerAngle = newPlayerAngle;
         }
 
         void ComputeGap()
         {
             m_CurrentGapPercentage = Mathf.Abs(m_CurrentPlayerAngle - m_TargetAngle) / (2*Mathf.PI);
-
-            Debug.Log("Compute gap before correcting : \nCurrentPlayer: " + m_CurrentPlayerAngle + "\nTargetAngle: " + m_TargetAngle + "\nGap: " + m_CurrentGapPercentage);
 
             // Get the acute angle
             if (m_CurrentGapPercentage > 0.5f)
@@ -66,21 +88,18 @@ namespace MayoMatic
 
         void DisplayPlayer()
         {
-            if (m_PlayerLineRenderer)
+            if (m_PlayerTransform)
             {
-                Vector3[] positions = { Vector3.zero, (new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0)).normalized * 2 };
-                m_PlayerLineRenderer.SetPositions(positions);
-
-                Color playerColor = Color.green * (1 - m_CurrentGapPercentage) + Color.red * m_CurrentGapPercentage;
-                m_PlayerLineRenderer.startColor = playerColor;
-                m_PlayerLineRenderer.endColor = playerColor;
+                m_PlayerTransform.localPosition = new Vector3(Input.GetAxis("Horizontal") * 2, Input.GetAxis("Vertical"), 0);
+                m_PlayerTransform.eulerAngles = m_PlayerInitialRotation + Vector3.forward * Input.GetAxis("Horizontal") * 10;
             }
         }
         void DisplayTarget()
         {
             if (m_TargetTransform)
             {
-                m_TargetTransform.eulerAngles = new Vector3(0, 0, m_TargetAngle * Mathf.Rad2Deg); 
+                m_TargetTransform.localPosition = new Vector3(0, Mathf.Sign(Mathf.Sin(m_TargetAngle)) *  0.5f + 0.5f * Mathf.Sin(m_TargetAngle), 0);
+                m_TargetTransform.localEulerAngles = new Vector3(0, 0, m_TargetAngle * Mathf  .Rad2Deg); 
             }
         }
 
@@ -100,6 +119,11 @@ namespace MayoMatic
         }
 
         public float GetCurrentGap()
+        {
+            return m_CurrentGapPercentage;
+        }
+
+        public float GetFramePlayerAngleSpeed()
         {
             return m_CurrentGapPercentage;
         }
